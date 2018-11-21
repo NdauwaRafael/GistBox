35014d4c2b612c83bf4a0978d5fc3e2717fb4f29
Raphael Karanja <rkaranja@cytonn.com>
2018-11-21 16:11:24 +0300

    display gist klist

--- js/components/Gist.js
+++ js/components/Gist.js
@@ -2,10 +2,16 @@
  * Created by Raphael Karanja on 21/11/2018.
  */
 var Gist = React.createClass({
+    getDefaultProps: function () {
+        return {
+            username: '',
+            url: ''
+        }
+    },
     render: function () {
         return (
             <div>
-                {this.props.username}'s' last Gist is <a href={this.props.url}>here</a>
+                {this.props.username}'s last Gist is <a href={this.props.url}>here</a>
             </div>
         );
     }
--- js/components/GistBox.js
+++ js/components/GistBox.js
@@ -2,9 +2,29 @@
  * Created by Raphael Karanja on 21/11/2018.
  */
 var GistBox = React.createClass({
+    getInitialState: function () {
+        return {
+            gists: [
+                {
+                    username: 'Raphael Karanja',
+                    url: 'https://github.com/NdauwaRafael',
+                    id: 5
+                }
+            ]
+        }
+    },
+
     render: function () {
+        let newGist = function (gist) {
+            return <Gist username={gist.username} url={gist.url} />
+        };
         return (
-            <div></div>
+            <div>
+                <h1>GistBox</h1>
+                {this.state.gists.map(newGist)}
+            </div>
         );
     }
-})
+});
+
+React.render(<GistBox/>, document.querySelector("#app"));
