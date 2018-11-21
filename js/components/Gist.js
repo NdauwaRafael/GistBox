72b6951d67aff8cc227a4db7a9dd6a242267d81c
Raphael Karanja <rkaranja@cytonn.com>
2018-11-21 15:43:57 +0300

    add GistBox component

--- js/components/Gist.js
+++ js/components/Gist.js
@@ -1,3 +1,12 @@
 /**
  * Created by Raphael Karanja on 21/11/2018.
  */
+var Gist = React.createClass({
+    render: function () {
+        return (
+            <div>
+                {this.props.username}'s' last Gist is <a href={this.props.url}>here</a>
+            </div>
+        );
+    }
+});
---
+++ js/components/GistBox.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Raphael Karanja on 21/11/2018.
+ */
+var GistBox = React.createClass({
+    render: function () {
+        return (
+            <div></div>
+        );
+    }
+})
