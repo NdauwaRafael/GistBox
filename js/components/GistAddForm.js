7634543524c35f743a4c01f11d23877050096c73
Raphael Karanja <rkaranja@cytonn.com>
2018-11-22 10:54:33 +0300

    send form information as an event slot to gitsbox component which uses the information to perform a http request from the server

--- js/components/GistAddForm.js
+++ js/components/GistAddForm.js
@@ -2,12 +2,25 @@
  * Created by Raphael Karanja on 22/11/2018.
  */
 var GistAddForm = React.createClass({
-
+    getInitialState: function () {
+        return {
+            text: ''
+        }
+    },
+    onChange: function (e) {
+        let text = e.target.value;
+        this.setState({text});
+    },
+    addGist: function (e) {
+        e.preventDefault();
+        this.props.onAdd(this.state.text);
+        this.setState({text: ''});
+    },
     render: function () {
         return (
             <div>
-                <form>
-                    <input type="text" placeholder="Type a GitHub username...."/>
+                <form onSubmit={this.addGist}>
+                    <input type="text" value={this.state.text} onChange={this.onChange} placeholder="Type a GitHub username...."/>
                     <button>Fetch Latest Gist</button>
                 </form>
             </div>
--- js/components/GistBox.js
+++ js/components/GistBox.js
@@ -2,15 +2,19 @@
  * Created by Raphael Karanja on 21/11/2018.
  */
 var GistBox = React.createClass({
+    addGist: function (username) {
+        let url = `https://api.github.com/users/${username}/gists`;
+        $.get(url,  (result)=> {
+            let username = result[0].owner.login;
+            let url = result[0].html_url
+
+            let gists = this.state.gists.concat({username, url});
+            this.setState({gists})
+        })
+    },
     getInitialState: function () {
         return {
-            gists: [
-                {
-                    username: 'Raphael Karanja',
-                    url: 'https://github.com/NdauwaRafael',
-                    id: 5
-                }
-            ]
+            gists: []
         }
     },
 
@@ -21,6 +25,7 @@ var GistBox = React.createClass({
         return (
             <div>
                 <h1>GistBox</h1>
+                <GistAddForm onAdd={this.addGist}/>
                 {this.state.gists.map(newGist)}
             </div>
         );
