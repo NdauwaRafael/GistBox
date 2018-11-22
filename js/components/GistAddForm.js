153c8aef89c48dd5f0951c6e4e0f32a810306d74
Raphael Karanja <rkaranja@cytonn.com>
2018-11-22 09:37:17 +0300

    add gistaddform form

--- js/components/GistAddForm.js
+++ js/components/GistAddForm.js
@@ -4,6 +4,13 @@
 var GistAddForm = React.createClass({
 
     render: function () {
-
+        return (
+            <div>
+                <form>
+                    <input type="text" placeholder="Type a GitHub username...."/>
+                    <button>Fetch Latest Gist</button>
+                </form>
+            </div>
+        )
     }
 });
